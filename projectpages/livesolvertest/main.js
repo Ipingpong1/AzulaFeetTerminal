c479906ca2e5212edc1091a2a70d764dd2acf59d
ipingpong1 <matiasortizdiez@gmail.com>
2021-07-15 05:21:02 -0400

    live solver wokr

--- projectpages/livesolvertest/main.js
+++ projectpages/livesolvertest/main.js
@@ -21,27 +21,28 @@ dy = y_vec[2]-y_vec[1]
 dt = 1*cfl*dx/c
 
 setTimeout(()=>{
-    u_mat = zeros3D(10000, x_vec.length, y_vec.length)
+    u_mat = zeros3D(1000, x_vec.length, y_vec.length)
     console.log('done')
     document.getElementById('inst').innerHTML = ('Click to place source')
 }, 1)
 
 blank_mat = zeros2D(x_vec.length, y_vec.length)
 
-t = 1
 placex = 0
 placey = 0
 sourceQ = false
 tempt = 0
 
+t = 1
 function draw(){
     if(started === true){
         if(sourceQ === true){
             if(t-tempt<100){
                 u_mat[t][Math.ceil(placex/dx)][Math.ceil(placey/dy)] = dt**2*Math.sin(t/10)
-                //u_mat[t][Math.ceil(x_vec.length/2)][Math.ceil(y_vec.length/2)] = dt**2*Math.sin(t/10)
+
             }else{
                 sourceQ = false
+
             }
         }
 
@@ -51,6 +52,7 @@ function draw(){
                 (((u_mat[t][x+1][y]-2*u_mat[t][x][y]+u_mat[t][x-1][y])*(1/dx**2))+
                 ((u_mat[t][x][y+1]-2*u_mat[t][x][y]+u_mat[t][x][y-1])*(1/dy**2)))+
                 2*u_mat[t][x][y]-u_mat[t-1][x][y]
+
             }
         }
 
@@ -59,15 +61,19 @@ function draw(){
                 if(u_mat[t][xx][yy]!=0){
                     if(u_mat[t][xx][yy]>0){
                         fill(clamp(u_mat[t][xx][yy]*500, 0, 255), 10-clamp(u_mat[t][xx][yy]*500, 0, 255), 255)
+
                     }
                     if(u_mat[t][xx][yy]<0){
                         fill(255,-50+clamp(-1*u_mat[t][xx][yy]*500, 0, 255), 100-clamp(-1*u_mat[t][xx][yy]*500, 0, 100))
+
                     }
                     noStroke()
                     rect(xx*dx, yy*dy, dx, dy)
+
                 }
             }
         }
+
         t++
     }
 }
